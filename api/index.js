eff22de6d54814aeb6199efaca3e4254d5c1a729
yllipetrovci <yllipetrovci@gmail.com>
2019-06-07 20:46:07 +0200

    temporary comment mongo connection

--- api/index.js
+++ api/index.js
@@ -8,12 +8,12 @@ const route = require('./routes/userRoute');
 const PORT = 3000;
 
 // connect to Mongo daemon
-mongoose.connect(
-    'mongodb://localhost:27017/admin',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// mongoose.connect(
+//     'mongodb://localhost:27017/admin',
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
 
 
 app.use(cors());
