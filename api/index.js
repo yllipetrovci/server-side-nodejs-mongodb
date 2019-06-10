101fb5cec452b35b7e40d8482ed37fd2d2cb3380
yllipetrovci <yllipetrovci@gmail.com>
2019-06-10 20:59:36 +0200

    add events method to get the events data for a specific user

--- api/index.js
+++ api/index.js
@@ -14,11 +14,27 @@ const PORT = 3000;
 const Event = require('./models/event');
 const User = require('./models/user');
 
-
+const events = eventIds => {
+    return Event.find({ _id: { $in: eventIds } })
+        .then(events => {
+            return events.map(event => {
+                return {
+                    ...event._doc,
+                    _id: event._id,
+                    creator: user.bind(this, event.creator)
+                }
+            })
+        }
+        ).catch(err => { throw err; })
+}
 
 const user = userId => {
     return User.findById(userId).then(user => {
-        return { ...user._doc, _id: user.id };
+        return {
+            ...user._doc,
+            _id: user.id,
+            createdEvents: events.bind(this, user._doc.createdEvents)
+        };
     }).catch(err => { throw err })
 }
 
