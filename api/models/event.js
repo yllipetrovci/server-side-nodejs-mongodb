39906df087fc73e5caa40e1b26295c39db16e075
yllipetrovci <yllipetrovci@gmail.com>
2019-06-11 21:07:11 +0200

    comment date

--- api/models/event.js
+++ api/models/event.js
@@ -15,10 +15,10 @@ const eventSchema = new Schema({
         type: Number,
         required: true
     },
-    date: {
-        type: Date,
-        required: true
-    },
+    // date: {
+    //     type: Date,
+    //     required: true
+    // },
     // creator: {
     //     type: Schema.Types.ObjectId,
     //     ref: 'User'
