9d0b21c8914f18f31e6f45fd4fa5bc25d0d4b290
yllipetrovci <yllipetrovci@gmail.com>
2019-06-12 21:16:22 +0200

    add single event function for booking

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -20,6 +20,19 @@ const events = async eventIds => {
     } catch (err) { throw err; }
 }
 
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId);
+        return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event.creator)
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
 const user = async userId => {
     try {
         const user = await User.findById(userId)
@@ -66,6 +79,8 @@ module.exports =
                     return {
                         ...booking._doc,
                         _id: booking.id,
+                        user: user.bind(this, booking._doc.user),
+                        event: singleEvent.bind(this,booking._doc.event),
                         createdAt: new Date().toISOString(),
                         updatedAt: new Date().toISOString(),
                     }
@@ -134,9 +149,11 @@ module.exports =
                 const result = await booking.save();
                 return {
                     ...result._doc,
-                    _id: result.id
+                    _id: result.id,
+                    createAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString()
                 }
-                
+
             } catch (err) {
                 throw err;
             }
