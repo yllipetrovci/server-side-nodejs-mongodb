d86e8972566d125ba28af33ef4055741c0a4c7d9
yllipetrovci <yllipetrovci@gmail.com>
2019-06-13 22:36:22 +0200

    refactor user resolvers

---
+++ api/graphql/resolvers/auth.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const User = require('../../models/user');
+
+module.exports = {
+    users: async () => {
+        try {
+            const users = await User.find()
+            return users.map(user => {
+                return { ...user._doc }
+            })
+
+        } catch (err) {
+            throw err;
+        }
+    },
+    createUser: async (args) => {
+        try {
+            const userResult = await User.findOne({ email: args.userInput.email })
+            //if a user exists with a same email address than throw an error
+            if (userResult) throw new Error("User exists already.");
+
+            //first argument its password, second argument its salting round
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12)
+
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            })
+
+            const result = await user.save();
+            return { ...result._doc, _id: result.id };
+
+        } catch (err) {
+            throw err;
+        }
+    },
+
+}
