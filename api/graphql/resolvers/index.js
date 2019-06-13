0cd6beeb419ee03b272ab6d337a1355cd3b55e76
yllipetrovci <yllipetrovci@gmail.com>
2019-06-13 22:05:52 +0200

    refactor booking return

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -13,6 +13,17 @@ const transformEvent = event => {
     }
 };
 
+const transformBooking = booking => {
+    return {
+        ...booking._doc,
+        _id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt),
+    }
+};
+
 const events = async eventIds => {
     try {
         const events = await Event.find({ _id: { $in: eventIds } })
@@ -72,14 +83,7 @@ module.exports =
             try {
                 const bookings = await Booking.find();
                 return bookings.map(booking => {
-                    return {
-                        ...booking._doc,
-                        _id: booking.id,
-                        user: user.bind(this, booking._doc.user),
-                        event: singleEvent.bind(this, booking._doc.event),
-                        createdAt: dateToString(booking._doc.createdAt),
-                        updatedAt: dateToString(booking._doc.updatedAt),
-                    }
+                    return transformBooking(booking);
                 })
             } catch (err) {
                 throw err;
